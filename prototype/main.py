da8d9f441952bcd47a1d1f054fe53d33801af467
QueensGambit <curry-berry@freenet.de>
2017-01-07 20:21:40 +0100

    added main.py which demonstrates the usage of the classifier as well as voabList.dump

---
+++ prototype/main.py
@@ -0,0 +1,26 @@
+"""
+@file: main.py.py
+Created on 07.01.2017 18:20
+@project: GitHubRepositoryClassifier
+
+@author: Anonym
+
+Sample usage of the repository-classifier
+"""
+
+from prototype import RepoClassifierNearestNeighbour
+
+
+repoClassNN = RepoClassifierNearestNeighbour()
+
+
+# strFilenameCSV = 'example_repos.csv'
+strFilenameCSV = 'additional_data_sets_cleaned.csv'
+
+repoClassNN.trainModel('/data/csv/' + strFilenameCSV)
+repoClassNN.exportModelToFile()
+# repoClassNN.loadModelFromFile()
+repoClassNN.predictResultsAndCompare()
+
+print('~~~~~~~~~~~~~ PREDICTION FROM SINGLE URL ~~~~~~~~~~~~~~~')
+repoClassNN.predictCategoryFromURL('https://github.com/akitaonrails/vimfiles')
