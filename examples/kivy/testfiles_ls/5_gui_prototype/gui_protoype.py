75b2ab0c3fca2d72efd80d38f015f5df034f1933
Lukas Scheuerle <lukas.scheuerle@web.de>
2017-01-08 15:28:13 +0100

    GUI plots pie chart at button press

--- examples/kivy/testfiles_ls/5_gui_prototype/gui_protoype.py
+++ examples/kivy/testfiles_ls/5_gui_prototype/gui_protoype.py
@@ -15,6 +15,11 @@ from kivy.properties import ObjectProperty
 from kivy.core.window import Window
 from kivy.uix.scrollview import ScrollView
 from kivy.properties import StringProperty
+from kivy.uix.button import Button
+from kivy.uix.widget import Widget
+from kivy.garden.matplotlib.backend_kivyagg import FigureCanvas
+
+import matplotlib.pyplot as plt
 
 kivy.require("1.9.0")
 
@@ -24,21 +29,56 @@ class ScrollableLabel(ScrollView):
 
 
 class GUILayout(BoxLayout):
-    url_input = ObjectProperty()
-    classifier_button = ObjectProperty()
-    error_label = ObjectProperty()
-    log_console = ObjectProperty()
+
+    # define the ObjectProperties to communicate with the .kv file
+    url_input = ObjectProperty()                    # user input line
+    classifier_button = ObjectProperty()            # the button above the user input. should start the process
+    error_label = ObjectProperty()                  # the label underneath the user input. short descriptions go here
+    log_console = ObjectProperty()                  # console to output logs. just add text to it.
+    result_label = ObjectProperty()                 # the big Result Label in the result corner
+    pie_chart = ObjectProperty()                    # the Layout for the piechart in the result corner
+    diagram1 = ObjectProperty()                     # the three TabbedPanelItems to put a diagram, expand if needed
+    diagram2 = ObjectProperty()                     # ↑
+    diagram3 = ObjectProperty()                     # ↑
+    dialabel1 = ObjectProperty()
 
     def classify_button_pressed(self):
         print("Button pressed!")
-        self.classifier_button.text = "pressed!"
+        self.classifier_button.text = "pressed!"                        # button demo
         self.classifier_button.disabled = True
-        self.error_label.text = "ERROR: Prototype not hooked up yet!"
+        self.error_label.text = "ERROR: Prototype not hooked up yet!"   # error label demo
         self.error_label.color = 1, 0, 0, 1
-        self.log_console.text = ""
+        self.log_console.text = ""                                      # clear console
         self.log_console.scroll_y = 0                                   # makes the console scroll down automatically
-        for i in range(0, 50):
+        for i in range(0, 50):                                          # demonstrate console
             self.log_console.text += ("Button pressed, " + str(i) + "\n")
+
+        button = Button(text='Testbutton')                              # demonstration of clearing an area and adding a
+        self.pie_chart.clear_widgets()                                  # widget to it
+        self.pie_chart.add_widget(button)
+
+
+        # ADDING A PIE CHART!
+        # The slices will be ordered and plotted counter-clockwise.
+        labels = 'Frogs', 'Hogs', 'Dogs', 'Logs'
+        sizes = [15, 30, 45, 10]
+        colors = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral']
+        explode = (0.1, 0, 0, 0)  # only "explode" the 1st slice (i.e. 'Dogs')
+        plt.figure(1)
+        plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+                autopct='%1.1f%%', shadow=True, startangle=90)
+        plt.axis('equal')
+        fig = plt.gcf()
+
+        #plt.show()
+
+        self.pie_chart.clear_widgets()
+        self.pie_chart.add_widget(FigureCanvas(fig))       # das hier sollte laufen
+
+
+
+
+
 
 
 
