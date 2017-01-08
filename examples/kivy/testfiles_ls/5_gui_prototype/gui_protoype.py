d58f4f5e62b3e6495f9d206a5bc1f693c1c81484
Lukas Scheuerle <lukas.scheuerle@web.de>
2017-01-08 13:29:34 +0100

    added a scrollable Console

--- examples/kivy/testfiles_ls/5_gui_prototype/gui_protoype.py
+++ examples/kivy/testfiles_ls/5_gui_prototype/gui_protoype.py
@@ -13,14 +13,21 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+from kivy.properties import StringProperty
 
 kivy.require("1.9.0")
+
+
+class ScrollableLabel(ScrollView):
+    text = StringProperty('')
 
 
 class GUILayout(BoxLayout):
     url_input = ObjectProperty()
     classifier_button = ObjectProperty()
     error_label = ObjectProperty()
+    log_console = ObjectProperty()
 
     def classify_button_pressed(self):
         print("Button pressed!")
@@ -28,6 +35,11 @@ class GUILayout(BoxLayout):
         self.classifier_button.disabled = True
         self.error_label.text = "ERROR: Prototype not hooked up yet!"
         self.error_label.color = 1, 0, 0, 1
+        self.log_console.text = ""
+        self.log_console.scroll_y = 0                                   # makes the console scroll down automatically
+        for i in range(0, 50):
+            self.log_console.text += ("Button pressed, " + str(i) + "\n")
+
 
 
 class GUIApp(App):
