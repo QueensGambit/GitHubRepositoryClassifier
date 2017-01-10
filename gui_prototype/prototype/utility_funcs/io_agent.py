c022f64c1039c0a1f215ecba7de74c81102b5f2d
QueensGambit <curry-berry@freenet.de>
2017-01-10 08:59:55 +0100

    added missing .py in utility_funcs/

---
+++ gui_prototype/prototype/utility_funcs/io_agent.py
@@ -0,0 +1,138 @@
+"""
+@file: io_agent.py
+Created on 11.12.2016 19:59
+@project: GitHubRepositoryClassifier
+
+@author: QueensGambit
+
+The InputOutputAgent loads data (json-Data, README...) from a given repository which
+ is defined by strUser and strName. If the needed data has already been requested before,
+ then is loaded from a file. Otherwise a new connection is created.
+ By default the autorization of the connection is done with an API-Token
+"""
+from _ast import In
+
+from clyent import json_help
+from docutils.io import Input
+
+import json
+import requests
+import base64
+
+# for installtion use:
+# pip install github3.py
+from github3 import GitHub
+from github3 import login
+import os
+
+#from io_agent import InputOutputAgent
+
+
+class InputOutputAgent:
+
+    gh = None
+    bWithToken = False
+
+    def __init__(self, strUser, strName):
+        """
+        Initializes the member variables
+
+        :param strUser: username of the owner of the repository
+        :param strName: name of the repository
+        :param bWithToken:  checks if a github-token shall be used for a higher api-limit
+
+        """
+
+        self.strUser = strUser
+        self.strName = strName
+
+        self.strAPIUrl = "https://api.github.com/repos/" + strUser + "/" + strName
+        self.lstReadmePath = {"https://raw.githubusercontent.com/" + strUser + "/" + strName + "/master/README.md",
+                              "https://raw.githubusercontent.com/" + strUser + "/" + strName + "/master/README.rst"}
+
+    def connectToGitHub(self):
+        if InputOutputAgent.gh is None:
+            if InputOutputAgent.bWithToken:
+                # the TokenGithubAPI is stored as an environment-variable
+                InputOutputAgent.gh = login(token=str(os.environ['TokenGithubAPI']))
+
+            else:
+                InputOutputAgent.gh = GitHub()
+
+            # https://github3py.readthedocs.io/en/master/
+            # InputOutputAgent.gh.refresh()
+            # InputOutputAgent.gh.refresh(True)  # Will send the GET with a header such that if nothing
+            # has changed, it will not count against your ratelimit
+            # otherwise you'll get the updated user object.
+
+            # get rate limit information
+            rates = InputOutputAgent.gh.rate_limit()
+            print('normal ratelimit info: ', rates['resources']['core'])  # => your normal ratelimit info
+            print('search ratelimit info: ', rates['resources']['search'])  # => your search ratelimit info
+
+    def loadJSONdata(self, strPathJSON):
+        """
+        loads the requested json-data either from a file or alternatively from the web
+        files are exported in the './json/' directory if they were requested
+        """
+
+        jsonAPI = None
+
+        # check if the json file has already been requested and was saved
+        if os.path.isfile(strPathJSON):
+            # read from it
+            with open(strPathJSON) as jsonData:
+                if jsonData is None:
+                    print("jsonData=None exception: ", strPathJSON)
+                jsonAPI = json.load(jsonData)
+        else:
+            self.connectToGitHub()
+            repo = InputOutputAgent.gh.repository(self.strUser, self.strName)
+            jsonAPI = repo.as_dict()  # .as_json() returns json.dumps(obj.as_dict())
+
+            # export to json-file
+            with open(strPathJSON, 'w') as outfile:
+                json.dump(jsonAPI, outfile)
+                print('json-data was exported to: ', strPathJSON)
+
+        return jsonAPI, self.strAPIUrl, self.lstReadmePath
+
+
+    # Get content from readme as string
+    def getReadme(self, strPathReadme):
+
+        # Create readme directory
+        if not os.path.exists(strPathReadme):
+            os.makedirs(strPathReadme)
+
+        strPathReadme += '\\' + self.strUser + '_' + self.strName + '.txt'
+
+        # Check if readme exists already. If so, open it.
+        if os.path.isfile(strPathReadme):
+            #print("Open readme..." )
+            return open(strPathReadme).read()
+
+        else:
+            self.connectToGitHub()
+            #print("Get readme...")
+
+            repo = InputOutputAgent.gh.repository(self.strUser, self.strName)
+            code64readme = repo.readme().content
+
+            # If the content of the received readme is a string and not a NullObject create
+            # a new file in directory. Otherwise create an empty file to prevent checking a
+            # repo twice.
+            if isinstance(code64readme, str):
+                strReadme = str(base64.b64decode(code64readme))
+
+            else:
+                strReadme = ""
+
+            file = open(strPathReadme, "w")
+            file.write(strReadme)
+            print('readme was exported to: ', strPathReadme)
+
+            return strReadme
+
+
+
---
+++ gui_prototype/prototype/utility_funcs/preprocessing_operations.py
@@ -0,0 +1,78 @@
+from sklearn.feature_extraction.text import CountVectorizer
+from wordcloud import WordCloud, STOPWORDS
+import os
+import pickle
+import logging
+
+def createVoabularyFeatures(lstRepos):
+    # lstAllReadmes = []
+
+    lstAllDescr = []
+
+    for tmpRepo in lstRepos:
+
+        # load the single lines to an array
+        #print('tmpRepo.getFilteredREADME(): ', tmpRepo.getFilteredREADME())
+        # lstAllReadmes.append(tmpRepo.getFilteredReadme())
+
+        lstAllDescr.append(tmpRepo.getFilteredRepoDescription())
+
+    lstBannedWordsAddition = ['git', 'repositori', 'github', 'new', 'us', 'use', 'high', 'nasa', 'present', 'open', 'public', 'http', 'www', 'com']
+
+    # create a counter which counts the occurrence of each word which is defined in the vocabulary
+    # by default the vocabulary consists of all words
+    vectorizer = CountVectorizer(min_df=3, stop_words=lstBannedWordsAddition)
+
+    # return a sparse matrix
+    # each column is mapped to a specific feature (see lstFeatureNames)
+    # the value describes the occurrence of the word in the current line
+    # matSparse = vectorizer.fit_transform(lstAllReadmes)
+    matSparse = vectorizer.fit_transform(lstAllDescr)
+
+    lstFeatureNames = vectorizer.get_feature_names()
+
+    # Generating word cloud image -> Only 20 plots are supported at once by default
+
+
+    return lstFeatureNames
+
+
+def readVocabFromFile(strVocabPath):
+    # http://stackoverflow.com/questions/899103/writing-a-list-to-a-file-with-python
+    # read dump file
+    with open(strVocabPath, 'rb') as fp:
+        logging.debug('open vocab from file...')
+        lstVoc = pickle.load(fp)
+
+    return lstVoc
+
+
+def initInputParameters(strVocabPath, lstGithubRepo):
+    """
+    Initialies the vocabulary set
+
+    :param strVocabPath:    path were the vocab list is stored
+    :param lstGithubRepo:   list of the githubRepository-objects
+    :return:
+    """
+
+    # generate or read the vocab, depending if the file already exists
+    lstVoc = []
+
+    if os.path.isfile(strVocabPath):
+        lstVoc = readVocabFromFile(strVocabPath)
+
+    else:
+        lstVoc = createVoabularyFeatures(lstGithubRepo)
+        # dump to file
+        with open(strVocabPath, 'wb') as fb:
+            logging.debug('dump vocab to file...')
+            pickle.dump(lstVoc, fb)
+
+    # for tmpGithubRepo in lstGithubRepo:
+    #     lstOccurence = tmpGithubRepo.getWordOccurences(lstVoc)
+    #     # print('sum(sparseMatrix):', sum(matSparse))
+    #     # print('lstOccurence: ', lstOccurence)
+    #     # print('len(Occurence): ', len(lstOccurence))
+
+    return lstVoc
