b1d7635a019cd24e4c55e065b7884a3cf4b4a613
QueensGambit <curry-berry@freenet.de>
2017-01-22 20:54:49 +0100

    added dll-loading for rc_cli

--- gui_prototype/rc_cli.py
+++ gui_prototype/rc_cli.py
@@ -9,6 +9,26 @@ Your description goes here...
 """
 
 
+# This block is needed to use the right dll-file for building
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+import ctypes
+import os
+import sys
+
+if getattr(sys, 'frozen', False):
+  # Override dll search path.
+  ctypes.windll.kernel32.SetDllDirectoryW('G:/Program Files/Anaconda3/Library/bin')
+  # Init code to load external dll
+  ctypes.CDLL('mkl_avx2.dll')
+  ctypes.CDLL('mkl_def.dll')
+  ctypes.CDLL('mkl_vml_avx2.dll')
+  ctypes.CDLL('mkl_vml_def.dll')
+
+  # Restore dll search path.
+  ctypes.windll.kernel32.SetDllDirectoryW(sys._MEIPASS)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
 strStopper1 = "=" * 79
 strStopper2 = "-" * 79
 
